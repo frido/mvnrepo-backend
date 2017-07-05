774ca19528f0f248e923af4eb1ef36c7ed24bb8d
Unknown <fridrich.peter@gmail.com>
2017-07-05 12:42:55 +0200

    mongodb

--- src/main/kotlin/frido/mvnrepo/backend/AppController.kt
+++ src/main/kotlin/frido/mvnrepo/backend/AppController.kt
@@ -6,15 +6,34 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RequestMapping
+import com.mongodb.DB
+import com.mongodb.MongoClient
+import com.mongodb.MongoClientURI
+
+
+
+
 
 @Controller
 @ComponentScan
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = arrayOf(org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration::class/*, org.springframework.boot.autoconfigure.mongo.MongoDataAutoConfiguration::class*/))
 class AppController {
+
+    @RequestMapping("test/")
+    @ResponseBody
+    fun test(): String {
+        val link = System.getenv("MONGO_URL")
+        print(link)
+        val uri = MongoClientURI(link)
+        val client = MongoClient(uri)
+        val db = client.getDatabase(uri.database)
+        println("Connect to database successfully")
+        return "ok"
+    }
 
 }
 
 fun main(args: Array<String>){
-    print("Hello World");
+    print("Hello World: ");
     SpringApplication.run(arrayOf(AppController::class.java), args);
 }
