3f63d59a982e0cae775488433aeedd9cac0fb5f6
Unknown <fridrich.peter@gmail.com>
2017-07-28 00:22:09 +0200

    lib controller

--- src/main/kotlin/frido/mvnrepo/backend/mongo/schema/Library.kt
+++ src/main/kotlin/frido/mvnrepo/backend/mongo/schema/Library.kt
@@ -1,5 +1,25 @@
 package frido.mvnrepo.backend.mongo.schema
 
-/**
- * Created by peter on 27.7.2017.
- */
+import org.bson.Document
+
+data class Library(
+        val id: String? = null,
+        val groupId: String? = null,
+        val artifactId: String? = null,
+        val version: String? = null,
+        val versioning: Versions? =null,
+        val latest: String? = null,
+        val description: String? = null,
+        val repo: String? = null
+) {
+    constructor(doc: Document) : this(
+            doc.getObjectId("_id").toString(),
+            doc.getString("groupId"),
+            doc.getString("artifactId"),
+            doc.getString("version"),
+            Versions(doc.getOrDefault(key = "versions", defaultValue = Document()) as Document),
+            doc.getString("latest"),
+            doc.getString("description"),
+            doc.getString("repo")
+    )
+}
--- src/main/kotlin/frido/mvnrepo/backend/mongo/schema/Versions.kt
+++ src/main/kotlin/frido/mvnrepo/backend/mongo/schema/Versions.kt
@@ -1,5 +1,17 @@
 package frido.mvnrepo.backend.mongo.schema
 
-/**
- * Created by peter on 27.7.2017.
- */
+import org.bson.Document
+
+data class Versions (
+    val latest: String? = null,
+    val release: String? = null,
+    val lastUpdated: String? = null,
+    val versions: List<String>? = null
+) {
+    constructor(doc: Document) : this(
+            doc.getString("latest"),
+            doc.getString("release"),
+            doc.getString("lastUpdated"),
+            doc.getOrDefault("versions", emptyList<String>()) as List<String>
+    )
+}
