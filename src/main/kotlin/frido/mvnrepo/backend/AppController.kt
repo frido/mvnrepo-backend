87e86149cee7187e75c6bf025bb174ab0d3bfef0
Peter Frido <fridrich.peter@gmail.com>
2018-02-28 10:33:37 +0000

    running via bootRun

--- src/main/kotlin/frido/mvnrepo/backend/AppController.kt
+++ src/main/kotlin/frido/mvnrepo/backend/AppController.kt
@@ -4,16 +4,23 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
-// TODO: upravit podla knihy
+// TODO: upravit podla knihy // TODO: Akej knihy
+@SpringBootApplication
 @ComponentScan(basePackages = arrayOf("frido.mvnrepo"))
 @EnableAutoConfiguration(exclude = arrayOf(org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration::class/*, org.springframework.boot.autoconfigure.mongo.MongoDataAutoConfiguration::class*/))
 @Configuration
 open class AppController {
-
+    //companion object {
+    //            @JvmStatic fun main(args: Array<String>) {
+    //                print("Hello World1: ");
+    //                SpringApplication.run(arrayOf(AppController::class.java), args);
+    //            }
+    //        }
 }
 
 fun main(args: Array<String>){
-    print("Hello World: ");
+    print("Hello World2: ");
     SpringApplication.run(arrayOf(AppController::class.java), args);
 }
